adee22e7e4b9435f422ff0004b3f4c44230b564c
minjiekim <72410435+minjiekim@users.noreply.github.com>
2021-04-01 12:38:12 -0700

    Add files via upload

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -5,84 +5,78 @@ import java.util.NoSuchElementException;
 import java.util.Stack;
 
 
-public class BinarySearchTree <T extends Comparable <? super T>> implements Iterable <T> {
-	
+public class BinarySearchTree<T extends Comparable<T>> implements Iterable<T> {
+	private int trueMods;
 	private BinaryNode root;
-	private int globalCounter;
-	
-	public BinarySearchTree(){
-		root = null;
-		globalCounter = 0;
+
+	// Most of you will prefer to use NULL NODES once you see how to use them.
+	private final BinaryNode NULL_NODE = new BinaryNode();
+
+	public BinarySearchTree() {
+		root = NULL_NODE; // NULL_NODE;
+		this.trueMods = 0;
 	}
-	
-	public BinarySearchTree(BinaryNode n){
-		root = n;
+
+	// For manual tests only
+	void setRoot(BinaryNode n) {
+		this.root = n;
 	}
 	
 	public boolean isEmpty() {
-		return root == null;
-	}
-	
-	public int height() {
-		if(isEmpty()) {
-			return -1;
-		}
-		return root.height();
+		return this.root == NULL_NODE;
 	}
 	
 	public int size() {
-		return toArrayList().size();
+		return root.size();
+	}
+	
+	public int height() {
+		return root.height();
+	}
+	
+	public boolean containsNonBST(T element) {
+		return root.containsNonBST(element);
+	}
+	
+	public ArrayList<T> toArrayList(){
+		ArrayList<T> list = new ArrayList<T>();
+		this.root.toArrayList(list);
+		return list;
 	}
 	
 	public String toString() {
 		return toArrayList().toString();
 	}
 	
-	public ArrayList<Object> toArrayList() {
-		if(isEmpty()) {
-			return new ArrayList<Object>();
-		}
-		return root.toArrayList();	
-	}
-	
 	public Object[] toArray() {
-		int size = size();
-		Object[] array= new Object[size];
-		for(int i = 0; i < size; i++) {
-			Object element = toArrayList().get(i);
-			array[i] = element;
-		}
-		return array;
-		
+		return toArrayList().toArray();
 	}
 	
-	public Iterator<T> preOrderIterator() {
-		return new PreOrderIterator();
-	}
-	
-	public boolean insert(T element) {
+	public boolean insert(T data) {
 		if(isEmpty()) {
-			root = new BinaryNode(element);
+			root = new BinaryNode(data);
 		}
-		globalCounter++;
-		return root.insert(element);
+		trueMods++;
+		return root.insert(data);	
 	}
 	
-	public  boolean remove(T element) {
-		MyBoolean bool = new MyBoolean();
-		
-		if(element == null) {
-			throw new IllegalArgumentException();
-		}
-		
+	public boolean remove(T data) {
 		if(isEmpty()) {
 			return false;
 		}
 		
-		root = root.remove(element, bool);
-		globalCounter++;
+		MyBoolean bool = new MyBoolean();
+		root = root.remove(data, bool);
 		
+		trueMods++;
 		return bool.getBool();
+	}
+	
+	public boolean contains(T data) {
+		if(isEmpty()) {
+			return false;
+		}
+		return root.contains(data);
 	}
 	
 	@Override
@@ -90,128 +84,206 @@ public class BinarySearchTree <T extends Comparable <? super T>> implements Iter
 		return new InOrderIterator();
 	}
 	
-	public class BinaryNode {
-		private T element;
-		private BinaryNode leftChild;
-		private BinaryNode rightChild;
-		
-		public BinaryNode(T e){
-			this.element = e;
-			this.leftChild = null;
-			this.rightChild = null;		
+	public Iterator<T> preOrderIterator(){
+		return new PreOrderIterator();
+	}
+	
+	public Iterator<T> inefficientIterator() {
+		return new InefficientIterator();
+	}
+	
+	// Not private, since we need access for manual testing.
+	class BinaryNode {
+		private T data;
+		private BinaryNode left;
+		private BinaryNode right;
+
+		//just to build the NULL_NODE
+		public BinaryNode() {
+			this.data = null;
+			this.left = null;
+			this.right = null;
 		}
 		
-		public BinaryNode remove(T e, MyBoolean myBool) {
-			int compare = element.compareTo(e);
+		public BinaryNode remove(T data, MyBoolean bool) {
+			if(data == null) {
+				throw new IllegalArgumentException();
+			}
 			
-			if(compare > 0) {   // goes left
-				if(leftChild != null) {
-					leftChild = leftChild.remove(e, 												myBool);
+			int compare = this.data.compareTo(data);
+			
+			if(compare < 0) {//data is greater, so goes to right sub tree
+				if(this.right != NULL_NODE) {
+					this.right = this.right.remove(data, bool);
 					return this;
-				}	
-			}
-			
-			if(compare < 0) {    //goes right
-				if(rightChild != null) {
-					
-				rightChild = rightChild.remove(e, myBool);
-				return this;
 				}
-			}
-
-			if(compare == 0) {
+			}else if(compare > 0) {//data is less, so goes to left subtree
+				if(this.left != NULL_NODE) {
+					this.left = this.left.remove(data, bool);
+					return this;
+				}
+			}else if(compare == 0){//it is equal to what it is compared to
+				if(this.right == NULL_NODE && this.left == NULL_NODE){
+					return NULL_NODE;
+				}
 				
-				if(leftChild == null && rightChild == null) {
-					return null;
+				//removal procedure if node only has right child
+				if(this.left == NULL_NODE) {
+					return this.right;
 				}
-				if(leftChild == null) {
-					return rightChild;
+				//removal procedure if node only has left child
+				if(this.right == NULL_NODE) {
+					return this.left;
 				}
-				if(rightChild == null) {
-					return leftChild;
-				}
-				if(rightChild != null && leftChild != null) {
-					element =  leftChild.getMax();
-					leftChild = leftChild.remove(element, myBool);
+				
+				//removal procedure if node has two children
+				if(this.right != NULL_NODE && this.left != NULL_NODE) {
+					this.data = this.left.getMaxFromRight();
+					this.left = this.left.remove(this.data, bool);
 				}
 				return this;
 			}
 			
-			myBool.setFalse();
+			//if data was not removed
+			bool.hasChanged();
 			return this;
 		}
 		
-		public T getMax() {
-			if(rightChild == null) {
-				return element;
+		//
+		public T getMaxFromRight() {
+			if(this.right == NULL_NODE) {
+				return this.data;
 			}
-			return rightChild.getMax();
+			return this.right.getMaxFromRight();
 		}
-		
 
-		public boolean insert(T e) {
-			if(e == null) {
+		public boolean contains(T data) {
+			if(this == NULL_NODE) {
+				return false;
+			}
+			
+			int compare = this.data.compareTo(data);
+			
+			if(compare < 0) {//goes to right
+				return this.right.contains(data);
+			}else if(compare > 0) {//goes left
+				return this.left.contains(data);
+			}else if(compare == 0){
+				return true;
+			}else {
+				return false;
+			}
+		}
+
+		public boolean insert(T data) {
+			if(data == null) {
 				throw new IllegalArgumentException();
 			}
-			if(element.compareTo(e) < 0) {	
-				if(rightChild == null) {
-					rightChild = new BinaryNode(e);
-					return true;
-				}
-				return rightChild.insert(e);	
-			}
-			if(element.compareTo(e) > 0) {
-				if(leftChild == null) {
-					leftChild = new BinaryNode(e);
-					return true;
-				}
-				return leftChild.insert(e);
-			}
-			return false;
 			
+			int compare = this.data.compareTo(data);
+			
+			if(compare < 0) {//goes to right
+				if(this.right == NULL_NODE) {
+					this.right = new BinaryNode(data);
+					return true;
+				}
+				return this.right.insert(data);
+			}else if(compare > 0) {//goes left
+				if(this.left == NULL_NODE) {
+					this.left = new BinaryNode(data);
+					return true;
+				}
+				return this.left.insert(data);
+			}else {
+				return false;
+			}
 		}
-		
+
+		public BinaryNode(T element) {
+			this.data = element;
+			this.left = NULL_NODE;
+			this.right = NULL_NODE;
+		}
+
 		public int height() {
 			int lHeight = -1;
 			int rHeight = -1;
+			if(this == NULL_NODE) {
+				return -1;
+			}
 			
-			if(leftChild != null) {
-				lHeight = leftChild.height();
-			}
-			if(rightChild != null) {
-				rHeight = rightChild.height();
-			}
-			return (lHeight > rHeight) ? lHeight:rHeight + 1 ;
+			lHeight = this.left.height() + 1;
+			rHeight = this.right.height() + 1;
+			
+			return (lHeight > rHeight) ? lHeight : rHeight;
 		}
 		
-		public ArrayList<Object> toArrayList() {
-			ArrayList<Object> a = new ArrayList<Object>();
-			if(leftChild != null) {
-				a.addAll(leftChild.toArrayList());
+		public void toArrayList(ArrayList<T> list) {
+			if(this == NULL_NODE) {
+				return;
 			}
-			a.add(element);
-			if(rightChild != null) {
-				a.addAll(rightChild.toArrayList());
-			}
-			return a;
+			this.left.toArrayList(list);
+			list.add(data);
+			this.right.toArrayList(list);
 		}
-	} 
-	
-	private class InOrderIterator implements Iterator<T> {
 		
-		private Stack<BinaryNode> stack = new Stack<BinaryNode>();
-		private int counter;
-		private BinaryNode prev;
+		public int size() {
+			if(this == NULL_NODE) {
+				return 0;
+			}
+			return 1 + left.size() + right.size();
+		}
+		
+		public boolean containsNonBST(T element) {
+			if(this == NULL_NODE) {
+				return false;
+			}
+			
+			if(this.data.equals(element)) {
+				return true;
+			}
+			
+			return left.containsNonBST(element) || right.containsNonBST(element);			
+		}
 
-		public InOrderIterator() {
-			prev = null;
-			counter = globalCounter ;
-			if(root != null)
-			getAllLeft(root);
+		public T getData() {
+			return this.data;
 		}
-	
+
+		public BinaryNode getLeft() {
+			return this.left;
+		}
+
+		public BinaryNode getRight() {
+			return this.right;
+		}
+
+		// For manual testing
+		public void setLeft(BinaryNode left) {
+			this.left = left;
+		}
+		
+		public void setRight(BinaryNode right) {
+			this.right = right;
+		}	
+	}
+
+	/**
+	 * Iterates through the tree using the i
+	 * @author class
+	 *
+	 */
+	public class InefficientIterator implements Iterator<T>{
+		private ArrayList<T> list; 
+		private int index = -1;
+		
+		public InefficientIterator() {
+			this.list = toArrayList();
+		}
+
+		@Override
 		public boolean hasNext() {
-			return !(stack.isEmpty());
+			return index + 1 < list.size();
 		}
 
 		@Override
@@ -219,113 +291,151 @@ public class BinarySearchTree <T extends Comparable <? super T>> implements Iter
 			if(!hasNext()) {
 				throw new NoSuchElementException();
 			}
-			
-			if(counter != globalCounter) {
+			index++;
+			return list.get(index);
+		}
+	}
+	
+	/**
+	 * Iterates through the tree left, root, right (aka natural order)
+	 * @author kimmm
+	 *
+	 */
+	public class InOrderIterator implements Iterator<T>{
+		private Stack<BinaryNode> nodes = new Stack<BinaryNode>();
+		private BinaryNode parent;
+		private int mods;
+		
+		public InOrderIterator() {
+			this.parent = NULL_NODE;
+			this.mods = trueMods;
+			if(root != NULL_NODE) {
+				getAllLeftChilds(root);
+			}
+		}
+
+		@Override
+		public boolean hasNext() {
+			return !nodes.isEmpty();
+		}
+
+		@Override
+		public T next() {
+			if(!hasNext()) {
+				throw new NoSuchElementException();
+			}
+			if(mods != trueMods) {
 				throw new ConcurrentModificationException();
 			}
-			
-			prev = stack.pop();
-			
-			if(prev.rightChild != null) {
-				getAllLeft(prev.rightChild);
+			parent = nodes.pop();
+			if(parent.right != NULL_NODE) {
+				getAllLeftChilds(parent.right);
 			}
-			return prev.element;
+			
+			return parent.data;
 		}
 		
-		private void getAllLeft(BinaryNode element) {
-			stack.push(element);
-			if(element.leftChild != null) {
-				getAllLeft(element.leftChild);
-			}
-		}
-		
-		public void remove(){
-			if (prev == null) {
+		public void remove() {
+			if(parent == NULL_NODE) {
 				throw new IllegalStateException();
 			}
-			if(counter != globalCounter) {
-				throw new ConcurrentModificationException();
-			}
 			
-			BinarySearchTree.this.remove(prev.element);
-			counter ++;
-			prev = null;
+			BinarySearchTree.this.remove(parent.data);
+			parent = NULL_NODE;
+			mods++;
+		}
+		
+		//pushes all the left children of a node onto the stack recursively
+		public void getAllLeftChilds(BinaryNode node) {
+			nodes.push(node);
+			if(node.left != NULL_NODE) {
+				getAllLeftChilds(node.left);
+			}
 		}
 		
 	}
 	
-	private class PreOrderIterator implements Iterator {
-		
-		private Stack<BinaryNode> stack = new Stack<BinaryNode>();
-		private BinaryNode prev;
-		private int counter;
-		
+	/**
+	 * Iterates through the tree so that it is root, left, right
+	 * @author kimmm
+	 *
+	 */
+	public class PreOrderIterator implements Iterator<T>{
+		private Stack<BinaryNode> nodes = new Stack<BinaryNode>();
+		private BinaryNode parent;
+		private int mods;
+
 		public PreOrderIterator() {
-			counter = globalCounter;
-			prev = null;
-			if(root != null) {
-				stack.push(root);
+			this.parent = NULL_NODE;
+			this.mods = trueMods;
+			if(root != NULL_NODE) {
+				nodes.push(root);
 			}
 		}
 		
 		@Override
 		public boolean hasNext() {
-			return !(stack.isEmpty());
+			return !nodes.isEmpty();
 		}
 
 		@Override
 		public T next() {
-			if(hasNext() == false) {
+			if(!hasNext()) {
 				throw new NoSuchElementException();
 			}
-			
-			if(counter != globalCounter) {
+			if(mods != trueMods) {
 				throw new ConcurrentModificationException();
 			}
-			prev = stack.pop();
-			if(prev.rightChild != null) {
-				stack.push(prev.rightChild);
-			}
-			if(prev.leftChild != null) {
-				stack.push(prev.leftChild);
-			}
 			
-			return prev.element;
+			parent = nodes.pop();
+			if(parent.right != NULL_NODE) {
+				nodes.push(parent.right);
+			}
+			if(parent.left != NULL_NODE) {
+				nodes.push(parent.left);
+			}
+			return parent.data;
 		}
 		
 		public void remove() {
-			if(prev == null) {
+			if(parent == NULL_NODE) {
 				throw new IllegalStateException();
-			}
-			
-			if(counter != globalCounter) {
-				throw new ConcurrentModificationException();
-			}
-
-			BinarySearchTree.this.remove(prev.element);
-			counter++;
-			
-			if(prev.leftChild != null && prev.rightChild != null) {
-				stack.pop();
-				stack.pop();
-				stack.push(prev);
-			}
-			
-			prev = null;
-			
+			}		
+			BinarySearchTree.this.remove(parent.data);
+			parent = NULL_NODE;
+			mods++;
 		}
 		
 	}
-
-	private class MyBoolean {
-		private boolean bool = true;
-
-		public void setFalse() {
-			bool = false;
+	
+	/**
+	 * Wrapper Class for Boolean
+	 * @author kimmm
+	 *
+	 */
+	public class MyBoolean{
+		private boolean notChanged;
+		
+		public MyBoolean() {
+			this.notChanged = true;
+		}
+		
+		public void hasChanged() {
+			this.notChanged = false;
 		}
 		
 		public boolean getBool() {
-			return bool;
+			return this.notChanged;
 		}
 	}
+
+
+
+
+
+	
+
+
+	 
+
 }
